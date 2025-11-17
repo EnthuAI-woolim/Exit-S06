479aa1d340f31e32085afe3b28d144e5ae69c6cf
EnthuAI-woolim <markyu428@gmail.com>
2025-11-17 18:14:48 +0900

    feat: Player 기능 및 607호 창문 추가
    - Player 이동 및 시야 스크립트 업데이트
    - 607호 창문 프리팹 및 머티리얼 추가
    - 607호 조명 프리팹 수정
    - 607호 씬 업데이트
    - ProBuilder 및 프로젝트 설정 업데이트
    
    origin/feat/player 브랜치에서 선택적으로 가져옴

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -27,7 +27,11 @@ public class PlayerMove : MonoBehaviour
         // 달리기(Shift)
         float speed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : moveSpeed;
 
-        Vector3 move = transform.right * x + transform.forward * z;
+        Vector3 inputDir = new Vector3(x, 0f, z);
+        Vector3 move = transform.TransformDirection(inputDir);
+        move.y = 0f;
+
+        // Vector3 move = transform.right * x + transform.forward * z;
         cc.Move(move * speed * Time.deltaTime);
 
         // 점프(Space)
@@ -39,3 +43,56 @@ public class PlayerMove : MonoBehaviour
         cc.Move(velocity * Time.deltaTime);
     }
 }
+
+
+//using UnityEngine;
+
+//[RequireComponent(typeof(CharacterController))]
+//public class PlayerMove : MonoBehaviour
+//{
+//    public float moveSpeed = 4.5f;
+//    public float sprintSpeed = 7.0f;
+//    public float jumpHeight = 1.0f;
+//    public float gravity = -9.81f;
+
+//    CharacterController cc;
+//    Vector3 velocity;
+//    bool isGrounded;
+
+//    void Awake()
+//    {
+//        cc = GetComponent<CharacterController>();
+//    }
+
+//    void Update()
+//    {
+//        // 땅 체크
+//        isGrounded = cc.isGrounded;
+//        if (isGrounded && velocity.y < 0f)
+//            velocity.y = -2f;   // 바닥에 붙게 약간만 눌러줌
+
+//        // 입력
+//        float x = Input.GetAxis("Horizontal");
+//        float z = Input.GetAxis("Vertical");
+
+//        // 평면 입력 벡터 (y=0)
+//        Vector3 inputDir = new Vector3(x, 0f, z);
+
+//        // Shift 달리기
+//        float speed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : moveSpeed;
+
+//        // 플레이어 기준 방향으로 변환 (회전 반영)
+//        Vector3 move = transform.TransformDirection(inputDir);
+//        move.y = 0f; // 혹시 모를 y 성분 제거
+
+//        cc.Move(move * speed * Time.deltaTime);
+
+//        // 점프
+//        if (Input.GetButtonDown("Jump") && isGrounded)
+//            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+//        // 중력
+//        velocity.y += gravity * Time.deltaTime;
+//        cc.Move(velocity * Time.deltaTime);
+//    }
+//}
