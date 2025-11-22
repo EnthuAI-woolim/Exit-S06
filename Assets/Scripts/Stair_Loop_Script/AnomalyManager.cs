42960b239785e028c507b4d173b0b071daa4539a
EnthuAI-woolim <markyu428@gmail.com>
2025-11-22 20:47:28 +0900

    feat: 이상현상 프리팹 및 씬 설정

--- Assets/Scripts/Stair_Loop_Script/AnomalyManager.cs
+++ Assets/Scripts/Stair_Loop_Script/AnomalyManager.cs
@@ -39,7 +39,7 @@ public class AnomalyManager : MonoBehaviour
         if(count == 1)
         {
             // 랜덤 인덱스 생성 (0~7) 따로 설정 해줘야함 (하드코딩)
-            RandomIndex = Random.Range(0, 8); 
+            RandomIndex = Random.Range(0, 4); 
             var obj = anomalyObjects[RandomIndex];
             obj.SetActive(true);
             HasAnomaly = true;
